827c74e46cc4d838bf601ce547ba24d9a47ea70b
AnonynFranck <67350844+AnonynFranck@users.noreply.github.com>
2020-06-23 22:06:23 -0500

    Proyecto actualizado 23-06-20

--- AppGitNotas/AppGitNotas/MainPage.xaml.cs
+++ AppGitNotas/AppGitNotas/MainPage.xaml.cs
@@ -6,6 +6,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
+using System.IO;
+
 namespace AppGitNotas
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
@@ -13,9 +15,46 @@ namespace AppGitNotas
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        // Obtener el directorio local para almacenar el archivo notas.txt
+        string archivo = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notas.txt");
         public MainPage()
         {
             InitializeComponent();
+            if (File.Exists(archivo))
+            {
+                txteditor.Text = File.ReadAllText(archivo);
+            }
+        }
+
+        private void btnGrabar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                File.WriteAllText(archivo, txteditor.Text);
+                DisplayAlert("Exito", "Archivo grabado con exito", "Ok");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "Ok");
+            }
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            if (File.Exists(archivo))
+            {
+                try
+                {
+                    File.Delete(archivo);
+                    txteditor.Text = string.Empty;
+                    DisplayAlert("Exito", "Archivo eliminado con exito", "Ok");
+                }
+                catch (Exception ex)
+                {
+                    DisplayAlert("Error", ex.Message, "Ok");
+                }
+                txteditor.Text = string.Empty;
+            }
         }
     }
 }
